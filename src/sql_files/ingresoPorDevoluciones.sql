a7bb702b7ee02aa87f6b2ac10ae6ca9518174ad7
omontes <pulgamontes@gmail.com>
2014-03-31 16:29:14 -0600

    Listo revision de impresion!

--- src/sql_files/ingresoPorDevoluciones.sql
+++ src/sql_files/ingresoPorDevoluciones.sql
@@ -1,3 +1,3 @@
 SELECT SUM(D.TotalFacturado)
 FROM devolucion AS D
-WHERE D.Estado='A' AND D.Concepto ='Devolucion' AND D.Fecha BETWEEN ? AND ?;
+WHERE D.Estado='A' AND D.Concepto ='Devolucion' AND SUBSTRING(D.Fecha,1,10) BETWEEN ? AND ?;

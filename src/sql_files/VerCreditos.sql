e6073c082dae703677fbbeccd5ee3e0a0ad9b454
omontes <pulgamontes@gmail.com>
2014-04-02 09:55:13 -0600

    Se aumento la cantidad de facturas, apartados y creditos que se pueden ver!! de 100 a 1000

--- src/sql_files/VerCreditos.sql
+++ src/sql_files/VerCreditos.sql
@@ -4,4 +4,4 @@ FF.idFactura=F.idFactura AND FF.Estado='A'AND FF.idFactura=FP.idFactura AND FF.i
 FROM Factura AS F,FacturasPendientes AS FP, Persona AS C, Persona AS V, PagosFactPendientes AS PFP
 WHERE F.Concepto='Credito' AND F.Estado='A' AND F.idFactura=FP.idFactura AND F.idCliente=C.idPersona AND F.idVendedor=V.idPersona 
 AND F.idVersionFactura=FP.idVersionFactPendientes AND PFP.idFacturaPendiente = FP.idFactura AND PFP.idFacturaVersionPagosPend = FP.idVersionFactPendientes
-ORDER BY F.idFactura DESC LIMIT 100;
+ORDER BY F.idFactura DESC LIMIT 1000;
